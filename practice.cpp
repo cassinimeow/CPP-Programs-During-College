2f98074ee538328e3b0054152e6c14afbad06674
Elizander Aguila <elizanderaguila@gmail.com>
2025-03-18 21:18:15 +0800

    meowing

---
+++ practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x, y;
+
+// Asking for Input
+  cout << "Put a two digit Integer value: ";
+  cin >> x >> y;
+
+// Processing
+  int sum = x + y;
+
+// Output
+  cout << "\n\nThe sum of the two numbers is: "<< sum;
+  return 0;
+}
